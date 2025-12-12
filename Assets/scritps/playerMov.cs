47cab2d662576eb048524aad3774892053ace5dc
ramiLewin <ramiroescuelaeest@gmail.com>
2025-12-12 18:58:40 -0300

    6:57pm second commit
    - se crea un personaje
    - escenario de pro builder
    - primer script para el personaje

--- Assets/scritps/playerMov.cs
+++ Assets/scritps/playerMov.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class script1 : MonoBehaviour
+public class playerMov : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
